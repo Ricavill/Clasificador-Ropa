9eb2fc9fca70c05c460422782bc64f00f58c0ecb
wasantac <34143200+wasantac@users.noreply.github.com>
2022-01-13 15:03:02 -0500

    resnet 18

--- ropaIA.py
+++ ropaIA.py
@@ -5,8 +5,15 @@ from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import ZeroPadding2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Add
 from tensorflow.keras.layers import Flatten
+from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import SGD
 
 def define_model():
@@ -27,3 +34,69 @@ def define_model():
 	opt = SGD(lr=0.001, momentum=0.9)
 	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 	return model
+
+
+def identity_block(x,filter):
+    x_skip = x
+    #Layer 1
+    x = Conv2D(filter,(3,3),padding='same')(x)
+    x = BatchNormalization(axis=3)(x)
+    x = Activation('relu')(x)
+    #Layer 2
+    x = Conv2D(filter,(3,3),padding='same')(x)
+    x = BatchNormalization(axis=3)(x)
+    #Residue
+    x = Add()([x,x_skip])
+    x = Activation('relu')(x)
+    return x
+
+
+def convolutional_block(x, filter):
+    # copy tensor to variable called x_skip
+    x_skip = x
+    # Layer 1
+    x = Conv2D(filter, (3,3), padding = 'same', strides = (2,2))(x)
+    x = BatchNormalization(axis=3)(x)
+    x = Activation('relu')(x)
+    # Layer 2
+    x = Conv2D(filter, (3,3), padding = 'same')(x)
+    x = BatchNormalization(axis=3)(x)
+    # Processing Residue with conv(1,1)
+    x_skip = Conv2D(filter, (1,1), strides = (2,2))(x_skip)
+    # Add Residue
+    x = Add()([x, x_skip])     
+    x = Activation('relu')(x)
+    return x
+
+def ResNet18(shape = (32, 32, 3), classes = 10):
+    # Step 1 (Setup Input Layer)
+    x_input = Input(shape)
+    x = ZeroPadding2D((3, 3))(x_input)
+    # Step 2 (Initial Conv layer along with maxPool)
+    x = Conv2D(64, kernel_size=7, strides=2, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D(pool_size=3, strides=2, padding='same')(x)
+    # Define size of sub-blocks and initial filter size
+    block_layers = [2,2,2,2]
+    filter_size = 64
+    # Step 3 Add the Resnet Blocks
+    for i in range(4):
+        if i == 0:
+            # For sub-block 1 Residual/Convolutional block not needed
+            for j in range(block_layers[i]):
+                x = identity_block(x, filter_size)
+        else:
+            # One Residual/Convolutional Block followed by Identity blocks
+            # The filter size will go on increasing by a factor of 2
+            filter_size = filter_size*2
+            x = convolutional_block(x, filter_size)
+            for j in range(block_layers[i] - 1):
+                x = identity_block(x, filter_size)
+    # Step 4 End Dense Network
+    x = AveragePooling2D((2,2), padding = 'same')(x)
+    x = Flatten()(x)
+    x = Dense(512, activation = 'relu')(x)
+    x = Dense(classes, activation = 'softmax')(x)
+    model = Model(inputs = x_input, outputs = x, name = "ResNet34")
+    return model
